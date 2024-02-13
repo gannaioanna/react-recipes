604a2f2965c6b53095e9fb6b031d1d2c177d6744
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-13 14:49:59 +0400

    third commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Recipe } from './pages/Recipe';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/react-recipes'>
         <Header />
         <main className='container content'>
           <Switch>
